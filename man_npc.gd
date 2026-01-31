8d236c3688076cfc59e2383cb22ace7c2e7ba63f
Eepr Drmr <astonisherred@gmail.com>
2026-01-31 08:53:03 +0200

    Added man NPC scene

---
+++ man_npc.gd
@@ -0,0 +1,9 @@
+class_name NPC extends CharacterBody2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	#position += Vector2(randf_range(-1.5, 1.5), randf_range(-1.5, 1.5))
+	pass
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
