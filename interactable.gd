03fcabe853914b61867ceafdc6bf0f7500090482
novomiracle <novomiracle@gmail.com>
2026-01-31 13:04:24 +0200

    shaders

--- interactable.gd
+++ interactable.gd
@@ -5,9 +5,11 @@ func _physics_process(delta: float) -> void:
 			print("interacted")
 func _on_area_entered(area: Area2D) -> void:
 	if area.is_in_group("player"):
+		$Sprite2D.set_instance_shader_parameter("active", true)
 		interacting = true
 
 
 func _on_area_exited(area: Area2D) -> void:
 	if area.is_in_group("player"):
 		interacting = false
+		$Sprite2D.set_instance_shader_parameter("active", false)
