9825ae5acba15e0694df803ce945861435b1028f
novomiracle <novomiracle@gmail.com>
2026-02-01 13:50:41 +0200

    PATCH

--- Scenes/map.gd
+++ Scenes/map.gd
@@ -1,5 +1,6 @@
 extends Node2D
 var spawn:Node2D 
 func _ready() -> void:
-	spawn = get_tree().get_nodes_in_group("spawner")[main.random.randi_range(0,4)]
-	spawn.spawn_friend()
+	for i in 2:
+		spawn = get_tree().get_nodes_in_group("spawner")[main.random.randi_range(0,4)]
+		spawn.spawn_friend()
--- interactable.gd
+++ interactable.gd
@@ -27,10 +27,11 @@ func _on_area_exited(area: Area2D) -> void:
 		#main.player.interact()
 
 func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
-	if interacting && Input.is_action_just_pressed("interact"):
+	var yeah:bool = true
+	for i in main.player.items:
+		yeah = yeah && !i
+	if interacting && Input.is_action_just_pressed("interact") && yeah:
 		sound.play()
-
-	if Input.is_action_just_pressed("interact"):
 		interact()
 		main.player.interact()
 
