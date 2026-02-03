ec9488d219de24a8629b6eeac41e6d54d9eebb5c
novomiracle <novomiracle@gmail.com>
2026-02-03 21:39:56 +0200

    patch(old)

--- Scenes/map.gd
+++ Scenes/map.gd
@@ -1,6 +1,6 @@
 extends Node2D
 var spawn:Node2D 
 func _ready() -> void:
-	for i in 2:
+	for i in 3:
 		spawn = get_tree().get_nodes_in_group("spawner")[main.random.randi_range(0,4)]
 		spawn.spawn_friend()
--- special_person.gd
+++ special_person.gd
@@ -2,7 +2,7 @@ extends NPC
 signal you_found_me
 
 func set_tolerance():
-	tolerance = 150
+	tolerance = 180
 
 func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	if Input.is_action_just_pressed("interact") && selected:
