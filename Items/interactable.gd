7811e43f343b364db6062db52ea4c6e41b67f21c
Eepr Drmr <astonisherred@gmail.com>
2026-02-10 12:49:32 +0200

    Rearanges

--- Items/interactable.gd
+++ Items/interactable.gd
@@ -18,7 +18,7 @@ func _on_area_exited(area: Area2D) -> void:
 	if area.is_in_group("player"):
 		interacting = false
 		$Sprite2D.set_instance_shader_parameter("active", false)
-		
+
 
 #func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	#if interacting && Input.is_action_just_pressed("interact"):
